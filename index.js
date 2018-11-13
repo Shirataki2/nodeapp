533da930e922831ea482e47a14f9571d7b2e5792
Shirataki2 <ahahah114514@gmail.com>
2018-11-14 02:15:03 +0900

    add unity app v0.0

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello, World!</h1>');
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.use(express.static('.'));
+
+io.on('connection', (socket) => {
+  console.log('A User connected');
+  socket.broadcast.emit('hi');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('pin', (data) => {
+    console.log(data);
+    socket.emit('pon', "Yay! You're on Websocket!");
+  });
+
+  socket.on('pushPosition', (data) => {
+    console.log(`Data Get:${data}`);
+  })
 });
 
 http.listen(3000, () => {
